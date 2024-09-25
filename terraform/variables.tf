976ae1475e9201a1e5cc9aeaef85d2c3a761c324
Anudeep <155814201+palemanudeep@users.noreply.github.com>
2024-09-25 15:50:18 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,29 +1,4 @@
 variable "region" {
-  description = "AWS region"
+  description = "The AWS region to deploy the resources"
   default     = "ap-south-1"
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  default     = "t2.large"
-}
-
-variable "ami" {
-  description = "AMI ID for the EC2 instance"
-  default     = "ami-0000791bad666add5"
-}
-
-variable "key_name" {
-  description = "Key pair name"
-  default     = "new"
-}
-
-variable "vpc_cidr" {
-  description = "CIDR block for VPC"
-  default     = "10.0.0.0/16"
-}
-
-variable "subnet_cidr" {
-  description = "CIDR block for Subnet"
-  default     = "10.0.1.0/24"
 }
