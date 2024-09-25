e3a1bbe408d61dd64315cc842618f35ab1c00d53
Anudeep <155814201+palemanudeep@users.noreply.github.com>
2024-09-25 16:07:01 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,73 +1,38 @@
-provider "aws" {
-  region = "ap-south-1"  # Permanent AWS region
-}
 
-resource "aws_vpc" "medusa_vpc" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "medusa-vpc"
-  }
-}
-
-resource "aws_subnet" "medusa_subnet" {
-  vpc_id            = aws_vpc.medusa_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-south-1a"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "medusa-subnet"
-  }
-}
-
-resource "aws_internet_gateway" "medusa_igw" {
-  vpc_id = aws_vpc.medusa_vpc.id
-  tags = {
-    Name = "medusa-igw"
-  }
-}
-
-resource "aws_route_table" "medusa_route_table" {
-  vpc_id = aws_vpc.medusa_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.medusa_igw.id
-  }
-
-  tags = {
-    Name = "medusa-route-table"
-  }
-}
-
-resource "aws_route_table_association" "medusa_route_table_association" {
-  subnet_id      = aws_subnet.medusa_subnet.id
-  route_table_id = aws_route_table.medusa_route_table.id
-}
-
-resource "aws_security_group" "medusa_sg" {
-  vpc_id = aws_vpc.medusa_vpc.id
+# Security Group for EC2
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-security-group"
+  description = "Allow SSH and HTTP"
+  vpc_id      = "vpc-0e77cbe60dfd345dd"
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  # SSH access (be cautious)
   }
 
   ingress {
     from_port   = 9000
     to_port     = 9000
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  # Medusa backend port
   }
 
+    # Allow HTTP traffic
   ingress {
-    from_port   = 7001
-    to_port     = 7001
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP access from anywhere
+  }
+
+  # Allow HTTPS traffic
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTPS access from anywhere
   }
 
   egress {
@@ -76,40 +41,21 @@ resource "aws_security_group" "medusa_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = {
-    Name = "medusa-sg"
-  }
 }
 
+# EC2 Instance
 resource "aws_instance" "medusa_instance" {
-  ami           = "ami-0000791bad666add5"  # Ubuntu AMI
-  instance_type = "t2.large"
-  key_name      = "new"  # EC2 Key Pair name
-  subnet_id     = aws_subnet.medusa_subnet.id
-  security_groups = [aws_security_group.medusa_sg.name]
-
-  root_block_device {
-    volume_size = 16  # 16 GB storage
-  }
-
+  ami           = "ami-0522ab6e1ddcc7055"
+  instance_type = "t2.xlarge"
+  key_name      = var.ec2_key_pair  # The key pair for SSH access
+  security_groups = [
+    aws_security_group.ec2_sg.name
+  ]
   tags = {
-    Name = "medusa-instance"
+    Name = "Medusa Backend"
   }
 }
 
-resource "aws_ecr_repository" "medusa_repo" {
-  name = "medusa-ecr-new"
-
-  tags = {
-    Name = "medusa-ecr"
-  }
-}
-
-output "ec2_public_ip" {
-  value = aws_instance.medusa_instance.public_ip
-}
-
-output "ecr_repository_url" {
-  value = aws_ecr_repository.medusa_repo.repository_url
+data "aws_instance" "medusa" {
+  instance_id = aws_instance.medusa_instance.id
 }
