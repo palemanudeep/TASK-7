044b60a079eaa7c77eded07f8677548ac1277210
Anudeep <155814201+palemanudeep@users.noreply.github.com>
2024-09-25 16:08:30 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,11 @@
-variable "region" {
-  description = "The AWS region to deploy the resources"
+
+variable "ec2_key_pair" {
+  description = "The name of the EC2 key pair for SSH access"
+  type        = string
+  default     = "new"
+}
+
+variable "aws_region" {
+  description = "The AWS region to deploy to"
   default     = "ap-south-1"
 }
