1ce9929bb5c7e2c793c19172800f484f6aa287d1
Anudeep <155814201+palemanudeep@users.noreply.github.com>
2024-09-25 15:36:04 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,9 @@
 output "ec2_public_ip" {
-  value = aws_instance.medusa_instance.public_ip
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.medusa_instance.public_ip
 }
 
 output "ecr_repository_url" {
-  value = aws_ecr_repository.medusa_repository.repository_url
+  description = "URL of the ECR repository"
+  value       = aws_ecr_repository.medusa_ecr.repository_url
 }
